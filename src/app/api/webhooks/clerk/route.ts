10657c3d3e91aff71e3abbdbb7dac28609449d6c
Patrick MacDonald <patrick@patmac.ca>
2026-05-13 20:32:44 -0600

    test api route

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -5,54 +5,56 @@ import {
   handleUserCreatedDal,
 } from "@/dal/webhooks";
 
-export async function POST(req: NextRequest) {
-  try {
-    const evt = (await verifyWebhook(req, {
-      signingSecret: process.env.CLERK_WEBHOOK_SIGNING_SECRET,
-    })) as WebhookEvent;
+export async function GET(req: NextRequest) {
+  
+  // try {
+  //   const evt = (await verifyWebhook(req, {
+  //     signingSecret: process.env.CLERK_WEBHOOK_SIGNING_SECRET,
+  //   })) as WebhookEvent;
 
-    console.log("Web Hook :", evt.type, " ", evt.data);
+  //   console.log("Web Hook :", evt.type, " ", evt.data);
 
-    try {
-      switch (evt.type) {
-        case "user.created": {
-          const data = evt.data;
-          const userId = data.id;
-          const userName =
-            `${data.first_name || ""} ${data.last_name || ""}`.trim() ||
-            data.username ||
-            "Personal Workspace";
-          const userEmail = data.email_addresses[0]?.email_address;
-          if (!userEmail) {
-            console.error("User created event without email address");
-            break;
-          }
-          handleUserCreatedDal(userId, userName, userEmail);
-          break;
-        }
+  //   try {
+  //     switch (evt.type) {
+  //       case "user.created": {
+  //         const data = evt.data;
+  //         const userId = data.id;
+  //         const userName =
+  //           `${data.first_name || ""} ${data.last_name || ""}`.trim() ||
+  //           data.username ||
+  //           "Personal Workspace";
+  //         const userEmail = data.email_addresses[0]?.email_address;
+  //         if (!userEmail) {
+  //           console.error("User created event without email address");
+  //           break;
+  //         }
+  //         handleUserCreatedDal(userId, userName, userEmail);
+  //         break;
+  //       }
 
-        case "organization.created": {
-          const orgId = evt.data.id;
-          const orgName = evt.data.name;
-          await handleOrganizationCreatedDal(orgId, orgName);
-          break;
-        }
+  //       case "organization.created": {
+  //         const orgId = evt.data.id;
+  //         const orgName = evt.data.name;
+  //         await handleOrganizationCreatedDal(orgId, orgName);
+  //         break;
+  //       }
 
-        default:
-          console.log(`Unhandled event type: ${evt.type}`);
-          break;
-      }
-    } catch (error) {
-      console.error("Error handling webhook event:", error);
-    }
+  //       default:
+  //         console.log(`Unhandled event type: ${evt.type}`);
+  //         break;
+  //     }
+  //   } catch (error) {
+  //     console.error("Error handling webhook event:", error);
+  //   }
 
-    return new Response("Webhook received", { status: 200 });
-  } catch (err) {
-    if (err instanceof Error) {
-      console.error("Error :", err.message);
-    } else {
-      console.error("Error :", err);
-    }
-    return new Response("Error", { status: 401 });
-  }
+  //   return new Response("Webhook received", { status: 200 });
+  // } catch (err) {
+  //   if (err instanceof Error) {
+  //     console.error("Error :", err.message);
+  //   } else {
+  //     console.error("Error :", err);
+  //   }
+  //   return new Response("Error", { status: 401 });
+  // }
+    return Response.json({ message: 'The route is working' })
 }
