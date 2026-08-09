33c83edad748ad7323901905a5ca6e529ea01c8e
Patrick MacDonald <patrick@patmac.ca>
2026-08-09 17:37:12 -0600

    clean up client list page

--- src/app/client-info-list/page.tsx
+++ src/app/client-info-list/page.tsx
@@ -17,46 +17,19 @@ export const metadata: Metadata = {
 export default async function ClientInfoListPage(
   props: PageProps<"/client-info-list">,
 ) {
-  const isAdminPromise = auth
-    .protect()
-    .then((authData) => authData.has({ role: "org:admin" }));
+  function parseParams(p: string | string[] | undefined): string | undefined {
+    return Array.isArray(p) ? p[0] : p;
+  }
+
   const clientsPromise = props.searchParams.then((params) =>
     getClientsForInfoDal(
-      Number(Array.isArray(params.page) ? params.page[0] : (params.page ?? 1)),
-      Array.isArray(params.search)
-        ? params.search[0]
-        : (params.search ?? undefined),
-      Array.isArray(params.clientId)
-        ? params.clientId[0]
-        : (params.clientId ?? undefined),
-    ).then((data) => data.clients),
-  );
-  const membersPromise = getOrganizationMembersDal();
-  const pagePromise = props.searchParams.then((params) =>
-    Number(Array.isArray(params.page) ? params.page[0] : (params.page ?? 1)),
+      Number(parseParams(params.page) ?? 1),
+      parseParams(params.search),
+      parseParams(params.clientId),
+    ),
   );
 
-  const searchPromise = props.searchParams.then((p) =>
-    String((Array.isArray(p.search) ? p.search[0] : p.search) ?? ""),
-  );
-
-  const clientIdPromise = props.searchParams.then((p) =>
-    String((Array.isArray(p.clientId) ? p.clientId[0] : p.clientId) ?? ""),
-  );
-
-  const totalPagesPromise = props.searchParams.then((params) =>
-    getClientsForInfoDal(
-      Number(Array.isArray(params.page) ? params.page[0] : (params.page ?? 1)),
-      Array.isArray(params.search)
-        ? params.search[0]
-        : (params.search ?? undefined),
-      Array.isArray(params.clientId)
-        ? params.clientId[0]
-        : (params.clientId ?? undefined),
-    ).then((data) => data.totalPages),
-  );
-
-  return (
+    return (
     <div className="container mx-auto max-w-7xl px-4 py-4 md:py-8">
       <PageHeader
         title="Clients"
@@ -66,19 +39,27 @@ export default async function ClientInfoListPage(
       <Suspense fallback={<ClientsSkeleton />}>
         <div id="client-list">
           <ClientInfoContainer
-            isAdminPromise={isAdminPromise}
-            clientsPromise={clientsPromise}
-            membersPromise={membersPromise}
-            searchPromise={searchPromise}
-            clientIdPromise={clientIdPromise}
+            isAdminPromise={auth
+              .protect()
+              .then((authData) => authData.has({ role: "org:admin" }))}
+            clientsPromise={clientsPromise.then((data) => data.clients)}
+            membersPromise={getOrganizationMembersDal()}
+            searchPromise={props.searchParams.then(
+              (p) => parseParams(p.search) ?? "",
+            )}
+            clientIdPromise={props.searchParams.then(
+              (p) => parseParams(p.clientId) ?? "",
+            )}
           />
         </div>
       </Suspense>
 
       <Suspense>
         <PaginationButtons
-          pagePromise={pagePromise}
-          totalPagesPromise={totalPagesPromise}
+          pagePromise={props.searchParams.then((params) =>
+            Number(parseParams(params.page) ?? 1),
+          )}
+          totalPagesPromise={clientsPromise.then((data) => data.totalPages)}
           hash="client-list"
         />
       </Suspense>
